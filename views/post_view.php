b67ca18760a4a28f98b178240ab4ddd4e706c992
Chamsmey <malwareattacker2022@gmail.com>
2022-03-15 15:09:25 +0700

    #3 display post HTML

--- views/post_view.php
+++ views/post_view.php
@@ -1,3 +1,50 @@
 <div class="container">
     <!-- Your code here -->
+
+    <div class="card mt-4">
+        <div class="card-header">
+            <div class="row d-flex">
+                <div class="col col-1 d-flex">
+                    <div class="card-profile">
+                        <img src="images/4043238_avatar_boy_kid_person_icon.png" class="rounded-circle float-start" alt="">
+                    </div>
+                
+                </div>
+                <div class="col col-3">
+                    <p class="h5">Smey Cham</p>
+                    <p class="text">2022-21-02 10AM</p>
+                </div>
+                <div class="col col-4"></div>
+
+                <div class="col col-3">
+                    <div class="icon-edit btn btn-primary ms-10"><i class="fa fa-edit"></i></div>
+                    <div class="btn btn-primary ms-3"><i class="fa fa-trash-o"></i></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <p class='discription'>រីករាយថ្ងៃខួបកំំណើត​អូនសម្លាញ់</p>
+
+            </div>
+            <div class="row class="rounded float-start"">
+                <img src="images/image-62296af7108246.04371375.jpg" class="rounded mx-auto d-block" alt="">
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="row">
+                <div class="col">
+                    <div class="btn-group">
+                        <i class="fa fa-thumbs-up btn btn-primary"></i>
+                        <i class="fa fa-comment btn btn-primary"></i>
+                    </div>
+                </div>
+               
+                
+                
+            </div>
+        </div>
+
+    </div>
 </div>
